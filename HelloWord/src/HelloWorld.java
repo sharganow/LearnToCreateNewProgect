1db8eca7c128a5e6c13368eea506870a1400032d
Andrey Sharganov <sharganow@gmail.com>
2022-06-13 23:25:43 +0500

    Add comments

--- HelloWord/src/HelloWorld.java
+++ HelloWord/src/HelloWorld.java
@@ -1,4 +1,15 @@
+/**
+ * A program that prints <code>Hello world!  </code>
+ *
+ * @author Andrey Sharganov
+ * @version 1.0
+ * */
 public class HelloWorld {
+    /**
+     * Program entry point.
+     *
+     * @param args command-line arguments
+     * */
     public static void main(String[] args){
         System.out.println("Hello world!!!");
     }
